67e67e875033d5b5b9e6c86c697b6326e61c45bc
Barnabas Chan <barny@post.com>
2016-07-14 09:52:23 +0800

    add readme file into game folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,10 +86,10 @@ $(document).ready(function() {
       console.log(this.vx);
 
       if ((this.vx === 1) && (this.x == 480)) {
-        this.x = -8;
+        this.x = -5;
         console.log(this.x);
       } else if ((this.vx === -1) && (this.x == -5)) {
-        this.x = 490;
+        this.x = 480;
       } else if ((this.vy === -1) && (this.y == -5)) {
         this.y = 490;
       } else if ((this.vy === 1) && (this.y == 490)) {
